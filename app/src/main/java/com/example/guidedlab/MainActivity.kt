da26ad22ddb7cab335247a9d2ca8cf37d9b7510a
matt <mkeyzer282@students.mchenry.edu>
2023-04-13 09:01:38 -0500

    initial commit

---
+++ app/src/main/java/com/example/guidedlab/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.example.guidedlab
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import org.w3c.dom.Text
+
+class MainActivity : AppCompatActivity() {
+
+    enum class LoginSuccess
+        constructor(val intValue: Int) {
+
+            login(1),
+            password(2),
+            success(0)
+
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val txtLogin = findViewById<TextView>(R.id.txtLoginEdit)
+        val txtPassword = findViewById<TextView>(R.id.txtPasswordEdit)
+        val txtButton = findViewById<Button>(R.id.txtButton)
+
+        txtButton.setOnClickListener{
+            when (CheckLogin(txtLogin.text.toString(), txtPassword.text.toString())){
+
+                LoginSuccess.login -> {
+                    Toast.makeText(applicationContext, getString(R.string.errMessageLogin), Toast.LENGTH_LONG).show()
+                    txtLogin.requestFocus()
+                }
+
+                LoginSuccess.password -> {
+                    Toast.makeText(applicationContext, getString(R.string.errMessagePassword), Toast.LENGTH_LONG).show()
+                    txtPassword.requestFocus()
+                }
+
+                else ->
+                    Toast.makeText(applicationContext, "success", Toast.LENGTH_LONG).show()
+
+            }
+        }
+    }
+
+    fun CheckLogin(txtLogin: String, txtPassword: String): LoginSuccess{
+        val holdLogin = "Matt"
+        val holdPass = "password"
+
+        if (holdLogin != txtLogin) {
+            return LoginSuccess.login
+        }
+        return if (holdPass != txtPassword) {
+            LoginSuccess.password
+        } else LoginSuccess.success
+    }
+}
